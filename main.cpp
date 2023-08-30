b1a29b1d17e9e257a514394f631b1d3e7584be72
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-08-30 03:54:19 +0330

    Update main.cpp main func

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <Windwos.h>
 #include <strings>
+
+using namespace std;
+
+DWORD pid; //process id
+
+DWORD Plyaer = 0xB6F5F0;
+DOWRD Money = 0XB7CE50;
+
+int main() {
+	
+}
