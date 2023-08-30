58c9c23a179d656c57d90622615eccaa52f7371c
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-08-30 03:55:05 +0330

    Read CJ Health

--- main.cpp
+++ main.cpp
@@ -10,5 +10,15 @@ DWORD Plyaer = 0xB6F5F0;
 DOWRD Money = 0XB7CE50;
 
 int main() {
-	
+	HWND hWND = FindWindowA(0, ("GTA: SAN ANDREAS"));
+
+	GetWindowsThreadProcessId(hWND, &pid);
+	HANDLE pHandle = OpenProcess(PROCESS_ALL_ACCESS, False, pid);
+
+	float hp;
+	ReadProcessMemory(pHandle, (LPVOID)Player, &Player, sizeof(Player), 0);
+	ReadProcessMemory(pHandle, (LPVOID)(Player + 0x540), &hp, sizeof(hp), 0);
+
+	cout << "Health : " << hp << endl;
+
 }
